cfda57ee41df97516104e0ae7d9b604d992af19c
blackmantram <misantropee@gmail.com>
2015-11-19 12:12:35 -0500

    added last score loading

---
+++ Assets/Scripts/LastScoreController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LastScoreController : MonoBehaviour {
+
+    void Start () {
+        LoadLastScore();
+    }
+
+    void LoadLastScore() {
+        DataPersistent.Load();
+		Text lastScoreText = GetComponent<Text>();
+		lastScoreText.text = "Your last score was : " + DataPersistent.data.lastScore;
+    }
+	
+}
